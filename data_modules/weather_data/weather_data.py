a28328dc9e04db9eb0d3f3f731af570eedc697aa
Diego Hermida Carrera <diego.hermida@udc.es>
2017-10-07 23:15:04 +0200

    Updated module.
     - Added "save_data" operation.
     - Changed data representation.
     - Added "_id" attribute.

--- data_modules/weather_data/weather_data.py
+++ data_modules/weather_data/weather_data.py
@@ -1,19 +1,38 @@
+import json
 import requests
 
-from util.util import get_config
+from util.db_util import connect
+from util.util import get_config, get_module_name
 
 config = get_config(__file__)
+module_name = get_module_name(__file__)
 
 
 def get_data():
+    """
+        Obtains data from the Wunderground API via HTTP requests. Currently only historical data is gathered.
+        N * M requests are made each time this function is called (N = number of tokens, M = max queries/min per token)
+
+        Parameters are read from configuration file (weather_data.config)
+
+        :return: A flat list of key-value objects.
+        :rtype: list
+    """
     data = []
     for token in config['TOKENS'] * config['MAX_REQUESTS_PER_MINUTE_AND_TOKEN']:
+        # TODO: By adding /conditions/forecast to BASE_URL gets more data in a single request
         url = config['BASE_URL'].replace('{TOKEN}', token).replace('{YYYYMMDD}', config['DATE']).replace('{LANG}',
             config['LANG']).replace('{STATE|COUNTRY}', config['COUNTRY']).replace('{CITY}', config['CITY'])
         r = requests.get(url)
-        data.append(r.content)
+        data.append(json.loads(r.content.decode('utf-8'))['history'])
     return data
+
+
+def save_data(data):
+    connection = connect(module_name)
+    connection.insert_many(data)
 
 
 if __name__ == '__main__':
     data = get_data()
+    save_data(data)
--- util/util.py
+++ util/util.py
@@ -18,10 +18,14 @@ def enum(*args):
 
 
 def get_config(path):
-    path = path.split('/')[-1].replace('.py', '.config')
+    path = path.replace('.py', '.config')
     with open(path, 'r') as f:
         config = yaml.load(f)
     return config
+
+
+def get_module_name(path):
+    return path.split('/')[-1].replace('.py', '')
 
 
 class Reader:
@@ -31,11 +35,3 @@ class Reader:
     def __call__(self, s):
         if not s.startswith('HDR'):
             self.data.append(s)
-
-
-if __name__ == '__main__':
-    pass
-    # uri = 'mongodb://climatechange_data_gathering_subsystem:TFG_Diego_Hermida_Carrera@localhost/'
-    # client = pymongo.MongoClient('127.0.0.1', 27017)
-    # client.admin.authenticate('climatechange_data_gathering_subsystem', 'TFG_Diego_Hermida_Carrera',
-    #                           mechanism='SCRAM-SHA-1')
