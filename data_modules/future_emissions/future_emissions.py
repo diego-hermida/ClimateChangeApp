2af9c98b97488aed1bf54d644f1303e8c139bdbf
Diego Hermida Carrera <diego.hermida@udc.es>
2017-10-07 23:13:39 +0200

    Updated module.
     - Added "save_data" operation.
     - Added "_id" attribute.

--- data_modules/future_emissions/future_emissions.py
+++ data_modules/future_emissions/future_emissions.py
@@ -1,15 +1,25 @@
-from util.util import get_config
+from util.db_util import connect
+from util.util import get_config, get_module_name
 
 config = get_config(__file__)
+module_name = get_module_name(__file__)
 
 
 def get_data():
+    """
+        Obtains data from the RPC Database. Documents have previously been downloaded and properly formatted.
+
+        Data location is read from configuration file (future_emissions.config)
+
+        :return: A flat list of key-value objects, containing parsed information from all files.
+        :rtype: list
+    """
     data = []
     for file in config['FILE_NAMES']:
         with open(config['DATA_DIR'] + file + config['FILE_EXT'], 'r') as f:
             for line in f:
                 fields = line.split()
-                d = {'year': fields[0], 'scenario': file, 'measures': []}
+                d = {'_id': fields[0] + '_' + file, 'year': fields[0], 'scenario': file, 'measures': []}
                 for (index, value) in enumerate(fields[1:]):
                     measure = {'measure': config['MEASURES'][index], 'value': value, 'units': config['UNITS'][index]}
                     d['measures'].append(measure)
@@ -17,5 +27,11 @@ def get_data():
     return data
 
 
+def save_data(data):
+    connection = connect(module_name)
+    connection.insert_many(data)
+
+
 if __name__ == '__main__':
     data = get_data()
+    save_data(data)
