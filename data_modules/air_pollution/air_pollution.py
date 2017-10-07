0cce781d9956ebcc3fc6fdc57053bfe7209dd5af
Diego Hermida Carrera <diego.hermida@udc.es>
2017-10-07 23:29:09 +0200

    Updated module.
     - Added "save_data" operation.

--- data_modules/air_pollution/air_pollution.py
+++ data_modules/air_pollution/air_pollution.py
@@ -1,29 +1,42 @@
+import json
 import requests
 
-from util.util import get_config
+from util.db_util import connect
+from util.util import get_config, get_module_name
 
 config = get_config(__file__)
+module_name = get_module_name(__file__)
 
 
 def get_data():
+    """
+        Obtains data from the WAQI API via HTTP requests.
+        L requests are made each time this function is called (L = number of locations)
+
+        Parameters are read from configuration file (air_pollution.config)
+
+        :return: A flat list of key-value objects.
+        :rtype: list
+    """
     data = []
     for loc in config['LOCATIONS']:
         url = config['BASE_URL'].replace('{LOC}', loc) + config['TOKEN']
         r = requests.get(url)
-        data.append(r.content)
+        data.append(json.loads(r.content.decode('utf-8')))
     return data
 
-    # r.content --> contiene el objeto JSON
-    # 'status' --> lo primero que hay que mirar. Puede ser:
-    #   'ok'
-    #   'error' (ahora hay que mirar 'message'):
-    #       'overQuota'
-    #       'invalidKey'
-    #       'unknownCity'
-    # Ejemplos:
-    #   - [b'{"status":"error","data":"Invalid key"}', b'{"status":"error","data":"Invalid key"}']
-    #   - [b'{"status":"error","message":"404"}', b'{"status":"error","message":"404"}']
+    # TODO: Check errors. Examples:
+    # TODO:  - [b'{"status":"error","data":"Invalid key"}', b'{"status":"error","data":"Invalid key"}']
+    # TODO:  - [b'{"status":"error","message":"404"}', b'{"status":"error","message":"404"}']
+
+
+def save_data(data):
+    connection = connect(module_name)
+    connection.insert_many(data)
 
 
 if __name__ == '__main__':
     data = get_data()
+    with open('file.txt', 'w') as f:
+        f.write(json.dumps(data, indent=4, separators=(',', ': ')))
+    #save_data(data)
