259a3066a8523c2e8e0fdb6306b5ae76689c3e52
Diego Hermida Carrera <diego.hermida@udc.es>
2017-10-07 23:14:28 +0200

    Updated module.
     - Added "save_data" operation.
     - Added "_id" attribute.

--- data_modules/sea_level_rise/sea_level_rise.py
+++ data_modules/sea_level_rise/sea_level_rise.py
@@ -1,17 +1,27 @@
 from ftplib import FTP
-
-
-from util.util import decimal_date_to_string, enum, get_config, Reader
+from util.db_util import connect
+from util.util import decimal_date_to_string, enum, get_config, get_module_name, Reader
 
 config = get_config(__file__)
+module_name = get_module_name(__file__)
+
 MeasureUnits = enum('mm')
 
 
 def get_data():
+    """
+        Obtains data from the NASA servers via FTP.
+
+        Parameters are read from configuration file (sea_level_rise.config)
+
+        :return: A flat list of key-value objects, containing information from all measures.
+        :rtype: list
+    """
     ftp = FTP(config['URL'])
     ftp.login()
     ftp.cwd(config['DATA_DIR'])  # Accessing directory
 
+    # File name changes every month, but it always starts with GMSL
     file_names = [x for x in ftp.nlst() if x.startswith('GMSL') and x.endswith(config['FILE_EXT'])]
     r = Reader()
 
@@ -20,13 +30,18 @@ def get_data():
     return to_JSON(r.data)
 
 
+def save_data(data):
+    connection = connect(module_name)
+    connection.insert_many(data)
+
+
 def to_JSON(data):
     json_data = []
     for line in data:
         fields = line.split()
         date = decimal_date_to_string(float(fields[2]), config['DATE_FORMAT'])
         altimeter = 'dual_frequency' if fields[0] == 0 else 'single_frequency'
-        measure = {'date': date, 'altimeter': altimeter, 'observations': fields[3],
+        measure = {'_id': date, 'date': date, 'altimeter': altimeter, 'observations': fields[3],
                    'weighted_observations': fields[4], 'measures': []}
         measure['measures'].append({'variation': fields[5], 'units': MeasureUnits.mm})
         measure['measures'].append({'deviation': fields[6], 'units': MeasureUnits.mm})
@@ -37,9 +52,9 @@ def to_JSON(data):
         measure['measures'].append({'smoothed_variation_GIA_annual_&_semi_annual_removed': fields[11],
                                     'units': MeasureUnits.mm})
         json_data.append(measure)
-    # Currently returns a list of elements in JSON notation
     return json_data
 
 
 if __name__ == '__main__':
     data = get_data()
+    save_data(data)
