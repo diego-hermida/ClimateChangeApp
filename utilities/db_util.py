6d9d49353dd3321889ef5ae85037f87c4715335f
Diego Hermida Carrera <diego.hermida@udc.es>
2017-10-31 11:19:56 +0100

    Added some utilities to be used by modules.
    - Saving and restoring state
    - Database access and queries
    - And more...

--- utilities/db_util.py
+++ utilities/db_util.py
@@ -4,6 +4,12 @@ from utilities.util import get_config
 
 
 def connect(collection_name=None):
+    """
+    Connects to a database and, if 'collection_name' isn't None, to a collection. Connection parameters are read from
+    a configuration file (db_util.config)
+    :param collection_name: Name of the collection to be connected to.
+    :return: A valid connection to a collection, or to a database (if 'collection_name' is None)
+    """
     config = get_config(__file__)
     client = pymongo.MongoClient(
         host=config['DB_SERVER'],
@@ -25,19 +31,38 @@ def connect(collection_name=None):
         raise ex
 
 
-def find(collection, last_id=None, count=None):
+def find(collection: str, fields=None, conditions=None, last_id=None, count=None, sort=None) -> dict:
     """
         Searches for data in a MongoDB collection. "_id" field is used to perform comparison between objects.
-        :param collection: A valid connection to a MongoDB connection.
-        :param last_id: Data will be retrieved from the next value of 'last_id' (or from the beginning if last_id is None)
+        :param collection: The collection name.
+        :param last_id: Data will be retrieved from 'last_id' next value (or from the beginning if last_id is None)
         :param count: Sets a maximum amount of values returned per function call (no limit if count is None).
-        :return: A list containing matching objects, or None if no data was found.
-        :rtype: list
+        :param fields: Fields to be selected. Any field to be selected is tagged with a '1'. Any field to be excluded is
+                       tagged with a '0'. '1's and '0's cannot be combined. Examples:
+                            - db.<table>.find({'_id': 1, 'name': 1, 'price':1}) --> SELECT id, name, price FROM <table>
+                            - db.<table>.find({'_id': 0}) --> SELECT <all fields but '_id' field> FROM <table>
+        :param conditions: Conditions that data must met to be selected. If "conditions" is None, a default condition
+                           is imposed if 'last_id' isn't None: '_id' field must be greater than 'last_id'.
+        :param sort: If not specified, uses a $natural sort. Otherwise, 'sort' can be a single string (ascending order
+                     by that field), or a list of tuples with the following format:
+                     [(field_1, <1 or -1>), ..., (field_N,  <1 or -1>)]. Examples:
+                            - db.<table>.find().sort('name') --> SELECT * FROM <table> ORDER BY 'name'
+                            - db.<table>.find().sort([('name', 1), ('price', -1)]) --> SELECT * FROM <table>
+                                                                                       ORDER BY 'name', 'price' DESC
+        :return: A dict with two values:
+                    - data: A list of values, or None if there were not values.
+                    - more: A boolean value, indicating whether retrieved data is the last or not.
+        :rtype: dict
     """
-    if last_id is None:
-        cursor = collection.find().sort('_id').limit(0 if count is None else count)  # 0 limit means "no limit"
-    else:
-        cursor = collection.find({'_id': {'$gt': last_id}}).sort('_id').limit(
-            0 if count is None else count)  # gt: greater than
+    collection = connect(collection)
+    if last_id:
+        if not conditions:
+            conditions = {}
+        conditions['_id'] = {'$gt': last_id}
+    cursor = collection.find(conditions, fields).sort(sort if sort else '$natural').limit(count + 1 if count else 0)
     data = [x for x in cursor]
-    return None if not data else data
+    data = {'data': data if data else None, 'more': len(data) > count if count else False}
+    if data['more']:
+        # Discarding last element (necessary to determine if there are more elements, but not requested)
+        data['data'] = data['data'][:-1]
+    return data
--- utilities/util.py
+++ utilities/util.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import yaml
 
 from abc import ABC, abstractmethod
@@ -21,18 +22,77 @@ def enum(*args):
 
 def get_config(path):
     path = path.replace('.py', '.config')
-    with open(path, 'r') as f:
+    with open(path, 'r', encoding='utf-8') as f:
         config = yaml.load(f)
     return config
+
+
+def read_state(path, repair_struct={}):
+    path = path.replace('.py', '.state')
+    with open(path, 'r') as f:
+        try:
+            state = json.load(f)
+        except json.decoder.JSONDecodeError:  # State is invalid, creating empty state according to REPAIR_STRUCT
+            f.close()
+            with open(path, 'w') as f2:
+                json.dump(repair_struct, f2)
+            return repair_struct
+    return state
+
+
+def write_state(state, path):
+    path = path.replace('.py', '.state')
+    with open(path, 'w') as f:
+        json.dump(state, f)
+
+
+def serialize_date(date: datetime):
+    return None if date is None else datetime.datetime.isoformat(date)
+
+
+def deserialize_date(date: str, date_format='%Y-%m-%dT%H:%M:%S.%f'):
+    return None if date is None else datetime.datetime.fromtimestamp(
+        datetime.datetime.strptime(date, date_format).timestamp())
 
 
 def get_module_name(path):
     return path.split('/')[-1].replace('.py', '')
 
 
-TimeUnits = enum('s', 'm', 'h', 'day', 'month', 'year', 'NEVER')
+TimeUnits = enum('s', 'm', 'h', 'day', 'week', 'month', 'year', 'NEVER')
 MeasureUnits = enum('mm', 'm', 'km', 'Gt')
 MassType = enum('antarctica', 'greenland', 'ocean')
+
+
+def worktime(date: datetime, frequency: dict) -> bool:
+    """
+    Given a date, adds a relative time measure (derived from "frequency" object)
+        :param date: Base date object. If "date" is None, True will always be returned.
+        :param frequency: A dict object ({value: int, units: TimeUnits}) to be added to "date".
+        :return: True if current timestamp is later than datetime plus timedelta.
+        :rtype: bool
+    """
+    if date is None:
+        return True
+    if frequency['units'] == TimeUnits.s:
+        min_work_date = date + relativedelta(seconds=frequency['value'])
+    elif frequency['units'] == TimeUnits.m:
+        min_work_date = date + relativedelta(minutes=frequency['value'])
+    elif frequency['units'] == TimeUnits.h:
+        min_work_date = date + relativedelta(hours=frequency['value'])
+    elif frequency['units'] == TimeUnits.day:
+        min_work_date = date + relativedelta(days=frequency['value'])
+    elif frequency['units'] == TimeUnits.week:
+        min_work_date = date + relativedelta(weeks=frequency['value'])
+    elif frequency['units'] == TimeUnits.month:
+        min_work_date = date + relativedelta(months=frequency['value'])
+    elif frequency['units'] == TimeUnits.year:
+        min_work_date = date + relativedelta(years=frequency['value'])
+    elif frequency['units'] == TimeUnits.NEVER:
+        return False
+    else:
+        raise ValueError('Unsupported TimeUnit value: ' + frequency['units'])
+    return min_work_date <= datetime.datetime.now()
 
 
 class Reader:
@@ -57,7 +117,7 @@ class DataCollector(ABC):
         pass
 
     @abstractmethod
-    def get_data(self):
+    def collect_data(self):
         pass
 
     @abstractmethod
@@ -67,40 +127,3 @@ class DataCollector(ABC):
     @abstractmethod
     def save_state(self):
         pass
-
-
-class TimeDelta:
-    def __init__(self, value: int, units: TimeUnits):
-        self.value = value
-        self.units = units
-
-    def to_dict(self) -> dict:
-        return {'value': self.value, 'units': self.units}
-
-
-def worktime(date: datetime, frequency: TimeDelta) -> bool:
-    """
-    Given a date, adds a relative time measure (derived from "frequency" object)
-        :param date: Base date object.
-        :param frequency: TimeDelta(value: int, units: TimeUnits) object, to be added to "date".
-        :return: True if current timestamp is later than datetime plus timedelta.
-        :rtype: bool
-    """
-    min_work_date = None
-    if frequency.units == TimeUnits.s:
-        min_work_date = date + relativedelta(seconds=frequency.value)
-    elif frequency.units == TimeUnits.m:
-        min_work_date = date + relativedelta(minutes=frequency.value)
-    elif frequency.units == TimeUnits.h:
-        min_work_date = date + relativedelta(hours=frequency.value)
-    elif frequency.units == TimeUnits.day:
-        min_work_date = date + relativedelta(days=frequency.value)
-    elif frequency.units == TimeUnits.month:
-        min_work_date = date + relativedelta(months=frequency.value)
-    elif frequency.units == TimeUnits.year:
-        min_work_date = date + relativedelta(years=frequency.value)
-    elif frequency.units == TimeUnits.NEVER:
-        return False
-    else:
-        raise ValueError('Unsupported TimeUnit value: ' + frequency.units)
-    return min_work_date <= datetime.datetime.now()
