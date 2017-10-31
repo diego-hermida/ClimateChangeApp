714859eb9bdf9953d2b1f70db58bc7cfc6c49fdb
Diego Hermida Carrera <diego.hermida@udc.es>
2017-10-31 11:22:03 +0100

    Implemented "Future emissions" as a stateful module.

--- data_modules/future_emissions/future_emissions.py
+++ data_modules/future_emissions/future_emissions.py
@@ -1,5 +1,8 @@
+import datetime
+
 from utilities.db_util import connect
-from utilities.util import DataCollector, get_config, get_module_name
+from utilities.util import DataCollector, get_config, get_module_name, read_state, write_state, worktime, \
+    serialize_date, deserialize_date
 
 __singleton = None
 
@@ -14,22 +17,34 @@ def instance() -> DataCollector:
 class __FutureEmissionsDataCollector(DataCollector):
     def __init__(self):
         super().__init__()
-        self.__data = []
+        self.__data = None
         self.__state = None
         self.__config = get_config(__file__)
         self.__module_name = get_module_name(__file__)
 
     def restore_state(self):
-        pass
+        """
+            Restores previous saved state (.state) if valid. Otherwise, creates a valid structure
+            for .state file from the STATE_STRUCT key in .config file.
+        """
+        self.__state = read_state(__file__, repair_struct=self.__config['STATE_STRUCT'])
+        self.__state['last_request'] = deserialize_date(self.__state['last_request'])  # Creates datetime object
 
     def worktime(self) -> bool:
-        pass
+        """
+            Determines whether this module is ready or not to perform new work, according to update policy and
+            last request's timestamp.
+            :return: True if it's work time, False otherwise.
+            :rtype: bool
+        """
+        return worktime(self.__state['last_request'], self.__state['update_frequency'])
 
-    def get_data(self):
+    def collect_data(self):
         """
             Obtains data from the RPC Database. Documents have previously been downloaded and properly formatted.
             Data location is read from configuration file (future_emissions.config)
         """
+        self.__data = []
         for file in self.__config['FILE_NAMES']:
             with open(self.__config['DATA_DIR'] + file + self.__config['FILE_EXT'], 'r') as f:
                 for line in f:
@@ -40,14 +55,29 @@ class __FutureEmissionsDataCollector(DataCollector):
                                    'units': self.__config['UNITS'][index]}
                         d['measures'].append(measure)
                     self.__data.append(d)
+        self.__state['last_request'] = datetime.datetime.now()
 
     def save_data(self):
         """
            Saves data into a persistent storage system (Currently, a MongoDB instance).
            Data is saved in a collection with the same name as the module.
         """
-        connection = connect(self.__module_name)
-        connection.insert_many(self.__data)
+        if self.__data is None:
+            pass
+        else:
+            connection = connect(self.__module_name)
+            connection.insert_many(self.__data)
 
     def save_state(self):
-        pass
+        """
+            Serializes state to .state file in such a way that can be deserialized later.
+        """
+        self.__state['last_request'] = serialize_date(self.__state['last_request'])
+        write_state(self.__state, __file__)
+
+    def __repr__(self):
+        return str(__class__.__name__) + ' [' \
+            '\n\t(*) config: ' + self.__config.__repr__() + \
+            '\n\t(*) data: ' + self.__data.__repr__() + \
+            '\n\t(*) module_name: ' + self.__module_name.__repr__() + \
+            '\n\t(*) state: ' + self.__state.__repr__() + ']\n'
