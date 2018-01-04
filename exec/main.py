d6733affb1401b65c99ed502dc3abfbd8581263a
Diego Hermida Carrera <diego.hermida@udc.es>
2018-01-05 00:46:00 +0100

    Implemented Main and Supervisor.
     - Changed packages.
     - Added features:
         - Each DataCollector works in its own thread.
         - Supervisor registers/unregisters DataCollectors.
         - Supervisor "fixes" erroneous executions.
         - Supervisor saves a "status report" of each execution.

---
+++ exec/main.py
@@ -0,0 +1,59 @@
+from data_collector.data_collector import DataCollectorThread, Message, MessageType
+from global_config.global_config import CONFIG
+from supervisor.supervisor import SupervisorThread
+from queue import Queue
+from timeit import default_timer as timer
+from threading import Condition
+from utilities.import_dir import import_modules
+from utilities.log_util import get_logger
+
+__condition = Condition()
+__channel = None
+
+if __name__ == '__main__':
+    start_time = timer()
+    # Getting logger instance
+    logger = get_logger(__file__, 'MainLogger', to_stdout=True, to_file=True)
+    logger.info('Starting Data Gathering Subsystem.')
+
+    # Dynamically, recursively imports all Python modules under base directory (and returns them in a list)
+    modules = import_modules(CONFIG['DATA_MODULES_PATH'], recursive=True,
+                             base_package=CONFIG['DATA_COLLECTOR_BASE_PACKAGE'])
+    if not modules:
+        from sys import exit
+
+        logger.critical('Data modules could not be instantiated. Aborting Data Gathering Subsystem.')
+        exit(1)
+
+    if len(modules) > CONFIG['MAX_RECOMMENDED_DATA_COLLECTORS']:
+        logger.warning('The number of DataCollector modules (%d) exceeds the recommended amount: %d. This may lead into'
+                ' performance penalties.'%(len(modules), CONFIG['MAX_RECOMMENDED_DATA_COLLECTORS']))
+
+    # Enabling a channel to pass messages between DataCollectors and Supervisor.
+    __channel = Queue(maxsize=(len(modules) * 2) + 2)
+    logger.debug('Created shared channel for message passing, with length %d.'%(__channel.maxsize))
+
+    supervisor = SupervisorThread(__channel, __condition)
+    supervisor.start()
+    logger.debug('Created Supervisor and started supervision.')
+
+    # Creates a Thread per DataCollector, and starts them.
+    threads = []
+    for module in modules:
+        thread = DataCollectorThread(module, __channel, __condition)
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        # Waits until all DataCollectors have finished execution.
+        thread.join()
+    # When all DataCollectors have finished execution, Supervisor is able to generate an execution report.
+    time = timer() - start_time
+    Message(MessageType.report, content=time).send(__channel, __condition)
+    # Once Supervisor has generated its report, it can exit.
+    Message(MessageType.exit).send(__channel, __condition)
+    supervisor.join()
+    # Logging total execution time (which is report time + report generation time + Supervisor's thread join)
+    m, s = divmod(timer() - start_time, 60)
+    h, m = divmod(m, 60)
+    logger.info('Data Gathering Subsystem has finished its execution. Total elapsed time is %d h, %d min and %d s.'%
+            (h, m, s))
--- supervisor/supervisor.py
+++ supervisor/supervisor.py
@@ -1,6 +1,11 @@
+import datetime
+import data_collector.data_collector as dc
+
+from pytz import UTC
 from queue import Queue
-from threading import Condition
-from utilities.util import get_config, get_module_name
+from threading import Condition, Thread
+from utilities.log_util import get_logger
+from utilities.util import get_config, read_state, serialize_date, write_state
 
 __singleton = None
 
@@ -8,32 +13,141 @@ __singleton = None
 def instance(queue: Queue, condition: Condition):
     global __singleton
     if __singleton is None:
-        __singleton = __Supervisor(queue, condition)
+        __singleton = Supervisor(queue, condition)
     return __singleton
 
 
-class __Supervisor:
+class SupervisorThread(Thread):
+    """
+        This class allows a Supervisor instance to be executed in its own thread.
+    """
     def __init__(self, queue: Queue, condition: Condition):
+        self.__supervisor = instance(queue, condition)
+        Thread.__init__(self)
+        self.setName('SupervisorThread')
+
+    def run(self):
+        try:
+            self.__supervisor.supervise()
+        except Exception:
+            self.__supervisor.logger.exception('Supervisor execution has been aborted due to an error.')
+
+
+class Supervisor:
+
+    def __init__(self, channel: Queue, condition: Condition):
         super().__init__()
-        self.__queue = queue
+        self.__channel = channel
         self.__condition = condition
-        self.__exit = False
-        self.__state = None
-        self.__config = get_config(__file__)
-        self.__module_name = get_module_name(__file__)
+        self.logger = get_logger(__file__, 'SupervisorLogger', to_stdout=True)
+        self.config = get_config(__file__)
+        self.state = read_state(__file__, repair_struct=self.config['STATE_STRUCT'])
+        self.registered = 0
+        self.unregistered = 0
+        self.registered_data_collectors = []
+        self.successful_executions = []
+        self.unsuccessful_executions = []
 
     def supervise(self):
-        while not self.__exit:
-            item = None
-            self.__condition.acquire()
+        self.logger.info('Starting module supervision.')
+        try:
             while True:
-                if not self.__queue.empty():
-                    item = self.__queue.get_nowait()
-                    break
-                self.__condition.wait()
-            self.__condition.release()
-            if isinstance(item, str) and item == self.__config['EXIT_MSG']:
-                self.__exit = True
+                message = None
+                self.__condition.acquire()
+                while True:
+                    if not self.__channel.empty():
+                        message = self.__channel.get_nowait()
+                        break
+                    self.__condition.wait()
+                self.__condition.release()
+                if not message:
+                    continue
+                try:
+                    assert isinstance(message, dc.Message)
+                except AssertionError:
+                    self.logger.warning('Messages should only be instances of the data_collector.Message class.')
+                if message.type == dc.MessageType.register:
+                    assert isinstance(message.content, dc.DataCollector)
+                    self.registered_data_collectors.append(message.content)
+                    self.registered += 1
+                    self.logger.debug('Registered DataCollector "%s".' % (message.content))
+                elif message.type == dc.MessageType.finished:
+                    assert isinstance(message.content, dc.DataCollector)
+                    self.unregistered += 1
+                    self.logger.debug('Unregistered DataCollector "%s".' % (message.content))
+                    self.verify_module_execution(message.content)
+                elif message.type == dc.MessageType.report:
+                    assert isinstance(message.content, float)
+                    self.logger.info('Generating execution report.')
+                    self.generate_report(message.content)
+                elif message.type == dc.MessageType.exit:
+                    if not self.__channel.empty():
+                        self.logger.warning('Supervisor should not receive EXIT signal before all DataCollectors have '
+                                'finished its execution.')
+                    raise StopIteration('EXIT')
+        except StopIteration:
+            self.logger.info('Supervisor has received EXIT signal. Exiting now.\n')
+
+    def verify_module_execution(self, data_collector: dc.DataCollector):
+        try:
+            states = data_collector.expose_transition_states(who=self)
+            assert states is not None
+            if states[-1] == dc.ABORTED and states[-2] >= dc.STATE_RESTORED and not data_collector.state[
+                    'restart_required']:
+                self.logger.warning('"%s" execution has been ABORTED, but module restart hasn\'t been '
+                        'scheduled. This issue will be fixed now.' % (data_collector))
+                try:
+                    data_collector.state['restart_required'] = True
+                    if data_collector.state['error']:
+                        data_collector.execute_actions(state=dc.EXECUTION_CHECKED, who=self)
+                    self.logger.info('Scheduled restart has been set for "%s". Errors and backoff time have been '
+                                       'serialized.' % (data_collector))
+                except Exception:
+                    self.logger.exception('Unable to schedule "%s" restart.' % (data_collector))
+            if data_collector.successful_execution():
+                self.successful_executions.append(str(data_collector))
             else:
-                data_collector = item.instance()
-                data_collector.auto_validate_state()  # Each module validates its state
+                self.unsuccessful_executions.append(str(data_collector))
+        except Exception:
+            self.logger.exception('An error occurred while verifying "%s" execution.' % (data_collector))
+
+    def generate_report(self, duration: float):
+        from json import dumps
+
+        modules_succeeded = 0
+        modules_failed = 0
+        collected_elements = 0
+        inserted_elements = 0
+        execution_succeeded = True
+        for dc in self.registered_data_collectors:
+            if dc.successful_execution():
+                modules_succeeded += 1
+            else:
+                execution_succeeded = False
+                modules_failed += 1
+            collected_elements += dc.state['data_elements'] if dc.state['data_elements'] else 0
+            inserted_elements += dc.state['inserted_elements'] if dc.state['inserted_elements'] else 0
+        self.state['last_execution']['duration'] = duration
+        self.state['last_execution']['modules_executed'] = len(self.registered_data_collectors)
+        self.state['last_execution']['modules_succeeded'] = modules_succeeded
+        self.state['last_execution']['modules_failed'] = modules_failed
+        self.state['last_execution']['collected_elements'] = collected_elements
+        self.state['last_execution']['inserted_elements'] = inserted_elements
+        self.state['last_execution']['execution_succeeded'] = execution_succeeded
+        self.state['last_execution']['timestamp'] = serialize_date(datetime.datetime.now(tz=UTC))
+        self.state['aggregated']['total_executions'] = self.state['aggregated']['total_executions'] + 1
+        self.state['aggregated']['total_execution_time'] = self.state['aggregated']['total_execution_time'] + duration
+        self.state['aggregated']['mean_duration'] = (self.state['aggregated']['mean_duration'] * (self.state[
+                'aggregated']['total_executions'] - 1) + duration) / (self.state['aggregated']['total_executions'])
+        self.state['aggregated']['total_succeeded_executions'] = self.state['aggregated'][
+                'total_succeeded_executions'] + 1 if execution_succeeded else self.state['aggregated'][
+                'total_succeeded_executions']
+        self.state['aggregated']['total_failed_executions'] = self.state['aggregated']['total_failed_executions'] \
+                + 1 if not execution_succeeded else self.state['aggregated']['total_failed_executions']
+        self.state['aggregated']['total_collected_elements'] = self.state['aggregated']['total_collected_elements'] \
+                + collected_elements
+        self.state['aggregated']['total_inserted_elements'] = self.state['aggregated']['total_inserted_elements'] \
+                + inserted_elements
+        path = write_state(self.state, __file__)
+        self.logger.info('An execution report has been saved to "%s".' % (path))
+        self.logger.debug('Execution results:\n%s'%(dumps(self.state, indent=4, separators=(',', ': '))))
