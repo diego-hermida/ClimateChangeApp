eaf2da671d02b222a723933275d75a975be7ac24
Diego Hermida Carrera <diego.hermida@udc.es>
2018-01-05 01:34:40 +0100

    Added global config.
     - Refactored files so that configuration values are accessible from a
       single point.

---
+++ global_config/global_config.py
@@ -0,0 +1,7 @@
+from os import environ
+
+from utilities.util import get_config
+
+CONFIG = get_config(__file__)
+CONFIG.update(get_config(__file__.replace('global_config.py', 'docker_global_config.py')) if not environ.get('DEV_MODE',
+        False) else get_config(__file__.replace('global_config.py', 'dev_global_config.py')))
