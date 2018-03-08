75ff8ce1da4f037c67ade1667c2099a19bee2d72
Diego Hermida Carrera <diego.hermida@udc.es>
2018-03-08 13:26:03 +0100

    Added missing function to main "install.sh" script

--- install.sh
+++ install.sh
@@ -31,7 +31,15 @@ function message () {
     tput sgr0;
 }
 
-
+function calculate_ip_address () {
+    local _ip _myip _line _nl=$'\n'
+    while IFS=$': \t' read -a _line ;do
+        [ -z "${_line%inet}" ] &&
+           _ip=${_line[${#_line[1]}>4?1:2]} &&
+           [ "${_ip#127.0.0.1}" ] && _myip=$_ip
+      done< <(LANG=C /sbin/ifconfig)
+    printf ${1+-v} $1 "%s${_nl:0:$[${#1}>0?0:1]}" $_myip
+}
 
 
 # ---------- Definitions ---------- #
