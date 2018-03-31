4919c751641ed4df9a28ac63098b32fc0c33fd56
Diego Hermida Carrera <diego.hermida@udc.es>
2018-03-31 14:09:43 +0200

    Refactored the `get_config` method to return empty dict on empty file by default.

--- utilities/config/config.py
+++ utilities/config/config.py
@@ -2,6 +2,5 @@ from utilities.util import get_config
 from os import environ
 
 UTIL_CONFIG = get_config(__file__)
-UTIL_CONFIG = UTIL_CONFIG if UTIL_CONFIG else {}
 UTIL_CONFIG.update(get_config(__file__.replace('config.py', 'docker_config.py')) if environ.get('DOCKER_MODE', False)
         else get_config(__file__.replace('config.py', 'dev_config.py')))
