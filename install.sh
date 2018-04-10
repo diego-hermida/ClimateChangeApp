b8252f92a08806dc77040c13dc3a7bca2d6bf4a6
Diego Hermida Carrera <diego.hermida@udc.es>
2018-04-10 10:30:00 +0200

    Finally fixed [BUG-040].

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ function usage () {
     exit_with_message 1 "Installs the application components.
             \n\n> usage: install.sh [-h] [--help] [--version] [--api-port xxxxx] [--hide-containers] [--host-ip xxx.xxx.xxx.xxx]
             \n\t[--macos] [--mongodb-port xxxxx] [--perform-deploy-actions] [--postgres-port xxxxx] [--root-dir <path>]
-            \n\t[--run-api] [--run-telegram] [--show-ip]
+            \n\t[--run-telegram] [--show-ip]
             \n • -h, --help: shows this message.
             \n • --version: displays app's version.
             \n • --api-port xxxxx: sets the exposed API port. Defaults to 5000.
@@ -23,7 +23,6 @@ function usage () {
             \n\t   steps are skipped.
             \n • --postgres-port xxxxx: sets the exposed PostgreSQL port. Defaults to 5432.
             \n • --root-dir <path>: installs the Application under a custom directory. Defaults to \"~/ClimateChangeApp\".
-            \n • --run-api: launches the API service after building it.
             \n • --run-telegram: launches the Telegram Configurator service after building it.
             \n • --show-ip: displays the IP address of the machine. If multiple IP's, displays them all.
             \n • --uid <val>: sets the UID of the user executing the Subsystem. Using \"0\" or \"root\" is not recommended.
@@ -41,7 +40,6 @@ MACOS=false;
 MONGODB_PORT=27017;
 POSTGRES_PORT=5432;
 ROOT_DIR=~/ClimateChangeApp;
-RUN_API=false;
 RUN_TELEGRAM=false;
 SKIP_DEPLOY=true;
 USER=$(id -u)
@@ -85,7 +83,6 @@ while getopts "$EXPECTED_INPUT" ARG; do
                     ensure_not_empty  "--root-dir" ${VAL};
                     ROOT_DIR=${VAL};
                 ;;
-                run-api) RUN_API=true ;;
                 run-telegram) RUN_TELEGRAM=true ;;
                 show-ip) show_ip_addresses ;;
                 uid)
@@ -178,6 +175,18 @@ if [ $? != 0 ]; then
     exit_with_message 1 "[ERROR] The MongoDB service could not be initialized." 1;
 fi
 
+# Getting internal IP address, if --hide-containers.
+if [$EXPOSE_CONTAINERS == "true" ]; then
+    MONGODB_IP=${HOST_IP};
+else
+    MONGODB_IP="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mongodb)"
+    if [ $? != 0 ]; then
+        exit_with_message 1 "[ERROR] Could not retrieve the local MongoDB IP address." 1;
+    else
+        message -1 "[INFO] Using \"$MONGODB_IP\" as the MongoDB IP address.";
+    fi
+fi
+
 
 # PostgreSQL component
 message 4 "[COMPONENT] Building and launching the PostgreSQL service.";
@@ -194,6 +203,18 @@ message -1 "[INFO] Launching the PostgreSQL service.";
 docker-compose up -d postgres;
 if [ $? != 0 ]; then
     exit_with_message 1 "[ERROR] The PostgreSQL service could not be initialized." 1;
+fi
+
+# Getting internal IP address, if --hide-containers.
+if [$EXPOSE_CONTAINERS == "true" ]; then
+    POSTGRES_IP=${HOST_IP};
+else
+    POSTGRES_IP="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgres)"
+    if [ $? != 0 ]; then
+        exit_with_message 1 "[ERROR] Could not retrieve the local PostgreSQL IP address." 1;
+    else
+        message -1 "[INFO] Using \"$POSTGRES_IP\" as the PostgeSQL IP address.";
+    fi
 fi
 
 
@@ -224,8 +245,8 @@ fi
 
 # Utilities component
 message 4 "[COMPONENT] Building and testing the Utilities component.";
-docker-compose build --build-arg MONGODB_IP=${HOST_IP} --build-arg MONGODB_PORT=${MONGODB_PORT} \
-                     --build-arg POSTGRES_IP=${HOST_IP} --build-arg POSTGRES_PORT=${POSTGRES_PORT} \
+docker-compose build --build-arg MONGODB_IP=${MONGODB_IP} --build-arg MONGODB_PORT=${MONGODB_PORT} \
+                     --build-arg POSTGRES_IP=${POSTGRES_IP} --build-arg POSTGRES_PORT=${POSTGRES_PORT} \
                      --build-arg DEPLOY_ARGS="${UTILITIES_DEPLOY_ARGS}" utilities;
 
 if [ $? != 0 ]; then
@@ -237,7 +258,7 @@ fi
 message 4 "[COMPONENT] Building the Data Gathering Subsystem.";
 
 # Building the Data Gathering Subsystem component
-docker-compose build --build-arg MONGODB_IP=${HOST_IP} --build-arg MONGODB_PORT=${MONGODB_PORT} \
+docker-compose build --build-arg MONGODB_IP=${MONGODB_IP} --build-arg MONGODB_PORT=${MONGODB_PORT} \
                      --build-arg DEPLOY_ARGS="${DATA_GATHERING_SUBSYSTEM_DEPLOY_ARGS}" \
                      --build-arg USER=${USER} data_gathering_subsystem;
 if [ $? != 0 ]; then
@@ -246,11 +267,7 @@ fi
 
 
 # API component
-if [ "$RUN_API" == "true" ]; then
-    message 4 "[COMPONENT] Building and launching the API service.";
-else
-    message 4 "[COMPONENT] Building the API service.";
-fi
+message 4 "[COMPONENT] Building and launching the API service.";
 
 # Deleting the API service if it was already been created: Brand-new container.
 if [ "$(docker ps -aq -f name=api_ci)" ]; then
@@ -261,18 +278,28 @@ fi
 
 # Building the API service
 message -1 "[INFO] Building the API image."
-docker-compose build --build-arg MONGODB_IP=${HOST_IP} --build-arg MONGODB_PORT=${MONGODB_PORT} \
+docker-compose build --build-arg MONGODB_IP=${MONGODB_IP} --build-arg MONGODB_PORT=${MONGODB_PORT} \
                      --build-arg DEPLOY_ARGS="${API_DEPLOY_ARGS}" --build-arg USER=${USER} api;
 if [ $? != 0 ]; then
     exit_with_message 1 "[INFO] The API image could not be built." 1;
 fi
 
 # Launching the API service
-if [ "$RUN_API" == "true" ]; then
-    message -1 "[INFO] Launching the API service.";
-    docker-compose up -d api;
+message -1 "[INFO] Launching the API service.";
+docker-compose up -d api;
+if [ $? != 0 ]; then
+    exit_with_message 1 "> The API service could not be initialized." 1;
+fi
+
+# Getting internal IP address, if --hide-containers.
+if [$EXPOSE_CONTAINERS == "true" ]; then
+    API_IP=${HOST_IP};
+else
+    API_IP="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' api)"
     if [ $? != 0 ]; then
-        exit_with_message 1 "> The API service could not be initialized." 1;
+        exit_with_message 1 "[ERROR] Could not retrieve the local API IP address." 1;
+    else
+        message -1 "[INFO] Using \"$API_IP\" as the API IP address.";
     fi
 fi
 
@@ -281,7 +308,7 @@ fi
 message 4 "[COMPONENT] Building the Data Conversion Subsystem.";
 
 # Building the Data Conversion Subsystem component
-docker-compose build --build-arg POSTGRES_IP=${HOST_IP} --build-arg API_IP=${HOST_IP} \
+docker-compose build --build-arg POSTGRES_IP=${POSTGRES_IP} --build-arg API_IP=${API_IP} \
                      --build-arg POSTGRES_PORT=${POSTGRES_PORT} --build-arg API_PORT=${API_PORT} \
                      --build-arg DEPLOY_ARGS="${DATA_CONVERSION_SUBSYSTEM_DEPLOY_ARGS}" \
                      --build-arg USER=${USER} data_conversion_subsystem;
@@ -293,11 +320,7 @@ fi
 # Displaying installation summary
 echo "";
 message 2 "[SUCCESS] Installation results:";
-if [ "$RUN_API" == "true" ]; then
-    message 2 "\t• API: up";
-else
-    message 2 "\t• API: built";
-fi
+message 2 "\t• API: up";
 message 2 "\t• Data Conversion Subsystem: built";
 message 2 "\t• Data Gathering Subsystem: built";
 message 2 "\t• MongoDB: up";
